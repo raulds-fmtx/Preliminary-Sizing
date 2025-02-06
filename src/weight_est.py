a3062f3d2af03509d7df4e743225e272dad22603
Raul Santos <rdsflomo03@gmail.com>
2025-02-06 08:51:10 -0600

    Estimation of solution space for weight estimate is created.

--- src/weight_est.py
+++ src/weight_est.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
 from parameters import params
 
 # Load CSV data
@@ -28,7 +29,7 @@ motor_power_model = LinearRegression().fit(x1, y)
 x1 = lipo_data['Energy (Wh)'].values.reshape(-1, 1)
 y = lipo_data['Energy Density (Wh/lb)']
 X = np.column_stack((x1))
-motor_power_density_model = LinearRegression().fit(x1, y)
+battery_power_density_model = LinearRegression().fit(x1, y)
 
 # Estimation functions
 def aircraft_weight_estimate(weight_battery,weight_motor):
@@ -41,10 +42,10 @@ def motor_power_estimate(thrust):
     return motor_power_model.predict([[thrust]])[0]
 
 def battery_power_density_estimate(thrust):
-    return motor_power_density_model.predict([[motor_power_estimate(thrust)]])[0]
+    return battery_power_density_model.predict([[motor_power_estimate(thrust)]])[0]
 
 # Calculate Weights, Thrust, and Power
-def weight_estimate(TW_ratio, Endurance, WTO_guess, WTO_lower, WTO_upper, show=True):
+def weight_estimate(TW_ratio, Endurance, WTO_guess, WTO_lower, WTO_upper):
     lower_init = WTO_lower
     upper_init = WTO_upper
     
@@ -62,42 +63,93 @@ def weight_estimate(TW_ratio, Endurance, WTO_guess, WTO_lower, WTO_upper, show=T
             WTO_upper = WTO_guess
         
         if np.abs(WTO_upper - lower_init) < 0.0001 or np.abs(WTO_lower - upper_init) < 0.0001:
-            return False
+            return None
         
         WTO_guess = (WTO_upper+WTO_lower)/2
     
-    if show:
-        print("Thrust-to-Weight Ratio:", TW_ratio)
-        print("Takeoff Weight:", WTO_guess, "lbs")
-        print("Thrust Req.:", TW_ratio * WTO_guess, "lbs")
-        print("Motor Weight:", motor_weight_estimate(T_guess), "lbs")
-        print("System Power:", motor_power_estimate(T_guess), "W")
-        print("Battery Weight:", Endurance * motor_power_estimate(T_guess) / battery_power_density_estimate(T_guess), "lbs")
-        print("Battery Energy:", Endurance * motor_power_estimate(T_guess), "Wh")
+    return [WTO_guess, 
+            motor_weight_estimate(T_guess),
+            Endurance * motor_power_estimate(T_guess) / battery_power_density_estimate(T_guess), 
+            motor_power_estimate(T_guess)]
     
-    return True
+# Get lists of parameters dependence on endurance and thrust-to-weight ratio   
+def full_estimation(thrust_weight_range, endurance_range, WTO_guess, WTO_lower, WTO_upper, num):
+    thrust_space = np.linspace(thrust_weight_range[0], thrust_weight_range[1], 1000)
+    endurance_space = np.linspace(endurance_range[0], endurance_range[1], num)
+    
+    takeoff_weight_list = []
+    motor_weight_list = []
+    battery_weight_list = []
+    power_list = []
+    tw_ratio_list = []
+    
+    for e in endurance_space:
+        takeoff_weight = []
+        motor_weight = []
+        battery_weight = []
+        power = []
+        tw_ratio = []
+        for t in thrust_space:
+            result = weight_estimate(t, e, WTO_guess, WTO_lower, WTO_upper) 
+            if result is not None:
+                takeoff_weight.append(result[0])
+                motor_weight.append(result[1])
+                battery_weight.append(result[2])
+                power.append(result[3])
+                tw_ratio.append(t)
+        takeoff_weight_list.append(takeoff_weight)
+        motor_weight_list.append(motor_weight)
+        battery_weight_list.append(battery_weight)
+        power_list.append(power)
+        tw_ratio_list.append(tw_ratio)
         
-def tw_ratio_range(tw_lower, tw_upper, num, Endurance, WTO_guess, WTO_lower, WTO_upper):
-    test_range = np.linspace(tw_lower, tw_upper, num)
-    valid_range = []
-    for i in test_range:
-        if weight_estimate(i,Endurance,WTO_guess,WTO_lower,WTO_upper,False):
-            valid_range.append(i)
-    print("T/W range:", [min(valid_range),max(valid_range)])
-
-# Paramaters
-Endurance = params["Endurance"] # hours
-TW_ratio = 0.6
-
-# Initial Guess for WTO
-WTO_guess = 10 # lbs
+    return takeoff_weight_list, motor_weight_list, battery_weight_list, power_list, tw_ratio_list
+      
+def visualize_estimation(endurance_range, takeoff_weight_list, motor_weight_list, battery_weight_list, power_list, tw_ratio_list):
+    fig, axs = plt.subplots(2,2)
+    fig.suptitle('Sizing Analysis')
+    
+    axs[0,0].set_ylabel('Takeoff Weight (lbs)')
+    axs[0,1].set_ylabel('Motor Weight (lbs)')
+    axs[1,0].set_ylabel('Battery Weight (lbs)')
+    axs[1,1].set_ylabel('Power (W)')
+    
+    for i in range(0,2):
+        for j in range(0,2):
+            for k in range(len(endurance_range)):
+                if (i,j) == (0,0):
+                    axs[i,j].plot(tw_ratio_list[k], takeoff_weight_list[k], label="{:.2f} mins".format(endurance_range[k]*60))
+                if (i,j) == (0,1):
+                    axs[i,j].plot(tw_ratio_list[k], motor_weight_list[k], label="{:.2f} mins".format(endurance_range[k]*60))
+                if (i,j) == (1,0):
+                    axs[i,j].plot(tw_ratio_list[k], battery_weight_list[k], label="{:.2f} mins".format(endurance_range[k]*60))
+                if (i,j) == (1,1):
+                    axs[i,j].plot(tw_ratio_list[k], power_list[k], label="{:.2f} mins".format(endurance_range[k]*60))
+            axs[i,j].legend()
+            axs[i,j].set_xlabel('Thrust-to-Weight Ratio')
+            axs[i,j].grid()
+    
+    plt.show()
+   
+# Weight Estimation sweep parameters
+tw_range = [0.1,1.5]
+endurance_range = [5/60.0,10/60.0]
+num = 6
+WTO_guess = 7 # lbs
 WTO_lower = 2 # lbs
-WTO_upper = 20 # lbs
- 
-# Range for T/W
-tw_lower = 0.1
-tw_upper = 1.5
-num = 10000
+WTO_upper = 12 # lbs
 
-tw_ratio_range(tw_lower,tw_upper,15,Endurance,WTO_guess,WTO_lower,WTO_upper)
-weight_estimate(TW_ratio,Endurance,WTO_guess,WTO_lower,WTO_upper)
+# Design Point
+endurance = 5
+TW_ratio = 0.6 
+
+# Run Weight Estimation
+takeoff_weight_list, motor_weight_list, battery_weight_list, power_list, tw_ratio_list = full_estimation(tw_range, endurance_range, WTO_guess, WTO_lower, WTO_upper, num)
+visualize_estimation(
+    np.linspace(endurance_range[0],endurance_range[1],num), 
+    takeoff_weight_list,
+    motor_weight_list, 
+    battery_weight_list, 
+    power_list, 
+    tw_ratio_list
+)
